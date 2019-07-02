b85e9ad00f8b7f31b0e1440ec8cd0aa5b5640c22
ethantanen <e_tanen@coloradocollege.edu>
2019-07-02 15:10:15 -0400

    eh

--- colorer.py
+++ colorer.py
@@ -1,9 +1,11 @@
 import os
+import schedule
+import time
 
 image = []
 
-def draw (n):
-    for i in range(n) :
+def draw ():
+    for i in range(10) :
         f = open('file.txt', 'a')
         f.write('\n' + str(i))
         os.system('git add .')
@@ -11,5 +13,9 @@ def draw (n):
         os.system('git push origin master')
         f.close()
 
+# schedule.every().day.at("1:30").do(draw)
+schedule.every().second.do(draw)
 
-draw(10)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
