a461c431ce3ccec2ed312e49642efbe1601a8c1e
ethantanen <e_tanen@coloradocollege.edu>
2019-07-02 13:49:34 -0400

    eh

--- colorer.py
+++ colorer.py
@@ -3,13 +3,13 @@ import os
 image = []
 
 def draw (n):
-    f = open('file.txt', 'w')
     for i in range(n) :
-        f.write(str(i))
+        f = open('file.txt', 'a')
+        f.write('\n' + str(i))
         os.system('git add .')
         os.system('git commit -m "eh"')
         os.system('git push origin master')
-    f.close()
+        f.close()
 
 
 draw(10)
